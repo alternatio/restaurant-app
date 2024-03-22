bcc4d9aeb2f06ceb0add192ddd81aef017f0e968
alternatio <90967598+alternatio@users.noreply.github.com>
2024-03-22 07:43:41 +0600

    fix: src

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -42,7 +42,7 @@ export default function Header({ user, setUser }: HeaderProps) {
 				)}
 			</div>
 			<Button className={style.menuButton} onClick={() => setMenuVisible(!menuIsVisible)}>
-				<img src="/public/menu-veggie-burger.svg" alt="veggie-burger-icon" />
+				<img src="../../../public/menu-veggie-burger.svg" alt="veggie-burger-icon" />
 			</Button>
 		</header>
 	)
